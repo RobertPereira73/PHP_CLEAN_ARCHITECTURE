eabc74accc50c0ed368c0cc008bbd836fed83c90
RobertPereira73 <robertpereira611@gmail.com>
2026-07-06 10:36:36 -0300

    feat: Testando estrutura

--- teste.php
+++ teste.php
@@ -8,4 +8,6 @@ $cpf = new CPF('49974396816');
 $email = new Email('robertpereira611@gmail.com');
 $aluno = new Aluno($cpf, $email, 'Robert');
 
+$aluno->addTelefone('12', '981715420');
+
 var_dump($aluno);
